d8ffadce4a17880c2a9dcbab557788c6f3f5a33c
Andrew <starandry83@mail.ru>
2024-10-14 07:27:23 +0300

    add babel for tests

--- jest.config.ts
+++ jest.config.ts
@@ -5,9 +5,10 @@ export default {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
     },
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Подмена стилей на mock
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
     testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 }
